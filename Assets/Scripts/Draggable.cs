57cbce7fd96d3f1c872847446ca6e20a0fa5b32d
Ido Adler <redturt@gmail.com>
2015-12-02 23:32:17 +0200

    size by mockup
    + potions returns to place

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -8,7 +8,6 @@ public class Draggable : MonoBehaviour {
 	float x;
 	float y;
 	Vector3 startPos;
-	bool free = true;
 
 	// Use this for initialization
 	void Start () {
@@ -22,18 +21,19 @@ public class Draggable : MonoBehaviour {
 	}
 		
 	void OnMouseDrag(){
-		if (free)
-			//GetComponent<Rigidbody2D>().MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-			transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(x,y,10.0f));
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		free = false;
-		transform.position = startPos;
-
-		//if (coll.gameObject.tag == "Enemy")
-		//coll.gameObject.SendMessage("ApplyDamage", 10);
-		//Destroy(this.gameObject);
+		transform.rotation = new Quaternion(0,0,.5f,1);
 	}
 
+	void OnTriggerExit2D(Collider2D other) {
+		transform.rotation = Quaternion.identity;
+	}
+
+	void OnMouseUp() {
+		transform.rotation = Quaternion.identity;
+		transform.position = startPos;
+	}
 }
